aeaaaca58877b6c67eeb51e0bc056888c92e2a5e
wizardrad@gmail.com <wizardrad@gmail.com>
2025-06-10 00:16:38 +0500

    Add search actor, customers info

--- db_read.py
+++ db_read.py
@@ -100,3 +100,79 @@ def get_films_by_year_and_genre(year: int, genre: str, limit: int = None):
     result = cursor.fetchall()
     close_connection(connection, cursor)
     return result
+
+
+def get_actor_by_film_title(title: str):
+    connection, cursor = make_connection(dbconfig=dbconfig_read)
+    if not connection:
+        return []
+
+    query = """
+        SELECT
+            a.first_name, a.last_name, f.title
+        FROM actor AS a
+        JOIN film_actor AS fa
+        ON a.actor_id = fa.actor_id
+        JOIN film as f
+        ON fa.film_id = f.film_id
+        WHERE f.title = %s;
+    """
+    cursor.execute(query, (title,))
+
+    result = cursor.fetchall()
+    close_connection(connection, cursor)
+    return result
+
+
+def get_customers_info(country: str, limit: int = None):
+    connection, cursor = make_connection(dbconfig=dbconfig_read)
+    if not connection:
+        return []
+
+    if limit is None or limit <= 0:
+        query = """
+            SELECT
+                ci.city,
+                co.country,
+                a.district,
+                a.address,
+                cus.first_name,
+                cus.last_name,
+                cus.email,
+                a.phone
+            FROM city AS ci
+            JOIN country AS co
+            ON ci.country_id = co.country_id
+            JOIN address AS a
+            ON ci.city_id = a.city_id
+            JOIN customer AS cus
+            ON a.address_id = cus.address_id
+            WHERE co.country = %s;
+        """
+        cursor.execute(query, (country,))
+    else:
+        query = """
+            SELECT
+                ci.city,
+                co.country,
+                a.district,
+                a.address,
+                cus.first_name,
+                cus.last_name,
+                cus.email,
+                a.phone
+            FROM city AS ci
+            JOIN country AS co
+            ON ci.country_id = co.country_id
+            JOIN address AS a
+            ON ci.city_id = a.city_id
+            JOIN customer AS cus
+            ON a.address_id = cus.address_id
+            WHERE co.country = %s
+            LIMIT %s;
+        """
+        cursor.execute(query, (country, limit))
+
+    result = cursor.fetchall()
+    close_connection(connection, cursor)
+    return result
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-from db_read import get_films_by_year, get_films_by_genre, get_films_by_year_and_genre
+from db_read import (get_films_by_year, get_films_by_genre, get_films_by_year_and_genre,
+                     get_actor_by_film_title, get_customers_info)
 from db_insert import log_search, get_clear_table
 from utils import get_limit
 
@@ -14,6 +15,8 @@ def main():
         1 - search by year
         2 - search by genre
         3 - search by year and genre
+        4 - search actor by film
+        5 - search info about customers by countries
         q - for exit program
         """)
         if choice == "0":
@@ -80,6 +83,41 @@ def main():
                     print("No films found in this combination")
             else:
                 print("Year is out of valid range")
+
+        elif choice == "4":
+            film_title = input("Enter film title to find actor info: ").upper()
+            actors = get_actor_by_film_title(film_title)
+
+            if actors:
+                for row in actors:
+                    first = str(row[0]).ljust(8)
+                    last = str(row[1]).ljust(9)
+                    title = str(row[2]).ljust(15)
+                    print(f"{first} | {last} | {title}")
+                log_search(search_type="actor info", search_value=film_title)
+            else:
+                print("No actors found for this film")
+
+        elif choice == "5":
+            country_name = input("Enter country to find info about customers from it: ").capitalize()
+            limit = get_limit()
+            customers = get_customers_info(country_name, limit)
+
+            if customers:
+                for row in customers:
+                    city = str(row[0]).ljust(25)
+                    country = str(row[1]).ljust(25)
+                    district = str(row[2]).ljust(25)
+                    address = str(row[3]).ljust(40)
+                    first_name = str(row[4]).ljust(10)
+                    last_name = str(row[5]).ljust(10)
+                    email = str(row[6]).ljust(10)
+                    phone = str(row[7]).ljust(12)
+                    print(f"{city} | {country} | {district} | {address} |"
+                          f"{first_name} | {last_name} | {email} | {phone}")
+                log_search(search_type="customers info", search_value=f"{country_name}")
+            else:
+                print("No customers found for this country")
 
         elif choice == "q":
             break
